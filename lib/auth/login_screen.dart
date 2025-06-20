d560ba9613270894bec9ca80a578c1fec335f7ca
Morison <morisonabinsinguza@gmail.com>
2025-06-20 17:59:56 +0300

    add login screen

---
+++ lib/auth/login_screen.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:smartspace/admin/admin_home_screen.dart';
+import 'package:smartspace/auth/auth_service.dart';
+import 'package:smartspace/auth/forgot_password_screen.dart';
+import 'package:smartspace/auth/register_screen.dart';
+import 'package:smartspace/buyer/buyer_home_screen.dart';
+import 'package:smartspace/seller/seller_home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthService _authService = AuthService();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  bool isPasswordHidden = true;
+
+  void _login() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    String? result = await _authService.login(
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (result == 'Admin') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const AdminHomeScreen()),
+      );
+    }
+    if (result == 'Seller'){
+        Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const SellerHomeScreen()),
+      );
+    }
+     else if (result == 'User') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const BuyerHomeScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login Failed: $result')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             
+              Image.asset('assets/logo.png'),
+              
+              // Email Input
+              _buildTextField("Email", _emailController),
+              const SizedBox(height: 20),
+
+              // Password Input
+              _buildTextField("Password", _passwordController,
+                  obscure: isPasswordHidden,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isPasswordHidden = !isPasswordHidden;
+                      });
+                    },
+                    icon: Icon(isPasswordHidden
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                  )),
+              const SizedBox(height: 25),
+
+              // Forgot Password
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ForgotPasswordScreen()),
+                  );
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 35.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Reset password",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Sign in Button
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: GestureDetector(
+                        onTap: _login,
+                        child: Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 181, 183, 74),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 19),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+              const SizedBox(height: 25),
+
+              // Register prompt
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => const RegisterScreen()), 
+                      );
+                    },
+                    child: const Text(
+                      " Register Now",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Reusable Input Field
+  Widget _buildTextField(String hint, TextEditingController controller,
+      {bool obscure = false, Widget? suffixIcon}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: TextField(
+            controller: controller,
+            obscureText: obscure,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hint,
+              suffixIcon: suffixIcon,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
