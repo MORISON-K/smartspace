23d104680c2ff88b74ff2477e07ca0cd9b33cbd9
Morison <morisonabinsinguza@gmail.com>
2025-06-20 18:00:21 +0300

    add login register screen

---
+++ lib/auth/register_screen.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:smartspace/auth/login_screen.dart';
+import 'package:smartspace/auth/auth_service.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final AuthService _authService = AuthService();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmpasswordController = TextEditingController();
+
+  String _selectedRole = "Select role";
+  bool _isLoading = false;
+  bool isPasswordHidden = true;
+
+  //Function to confirm if passwords match
+  bool passwordConfirmed() {
+    return _passwordController.text.trim() ==
+        _confirmpasswordController.text.trim();
+  }
+
+  //Dispose controllers properly
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmpasswordController.dispose();
+    super.dispose();
+  }
+
+  //Signup logic
+  void _signup() async {
+    if (!passwordConfirmed()) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Passwords do not match")));
+      return;
+    }
+
+    if (_selectedRole == "Select role") {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Please select a role")));
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    String? result = await _authService.signup(
+      name: _nameController.text,
+      email: _emailController.text,
+      password: _passwordController.text,
+      role: _selectedRole,
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (result != null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Signup Failed: $result")));
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/logo.png'),
+              const Text(
+                "Register Now with your details",
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 15),
+
+              // Name Input
+              _buildTextField("Name", _nameController),
+              const SizedBox(height: 15),
+
+              // Email Input
+              _buildTextField("Email", _emailController),
+              const SizedBox(height: 15),
+
+               // Role Dropdown
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: DropdownButtonFormField<String>(
+                  value: _selectedRole,
+                  decoration: const InputDecoration(
+                    labelText: 'Role',
+                    border: OutlineInputBorder(), // corrected
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedRole = newValue!;
+                    });
+                  },
+                  items:
+                      ['Select role', 'Seller', 'User']
+                          .map(
+                            (role) => DropdownMenuItem(
+                              value: role,
+                              child: Text(role),
+                            ),
+                          )
+                          .toList(),
+                ),
+              ), 
+              const SizedBox(height: 15),
+
+              // Password Input
+              _buildTextField(
+                "Password",
+                _passwordController,
+                obscure: isPasswordHidden,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isPasswordHidden ? Icons.visibility_off : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isPasswordHidden = !isPasswordHidden;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              // Confirm Password
+              _buildTextField(
+                "Confirm Password",
+                _confirmpasswordController,
+                obscure: true,
+              ),
+              const SizedBox(height: 15),
+
+              // Signup Button or Spinner
+              _isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: GestureDetector(
+                      onTap: _signup,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 181, 183, 74),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(color: Colors.white, fontSize: 19),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+              const SizedBox(height: 20),
+
+              // Already have account
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account? ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (_) => const LoginScreen()),
+                      );
+                    },
+                    child: const Text(
+                      " Login Now",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Reusable text field
+  Widget _buildTextField(
+    String hint,
+    TextEditingController controller, {
+    bool obscure = false,
+    Widget? suffixIcon,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: TextField(
+            controller: controller,
+            obscureText: obscure,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hint,
+              suffixIcon: suffixIcon,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
