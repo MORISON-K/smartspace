b05cd9ce4a6219ca5b52b62ca641455ccc7619a3
Morison <morisonabinsinguza@gmail.com>
2025-06-20 17:57:49 +0300

    add seller homepage

---
+++ lib/seller/seller_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SellerHomeScreen extends StatelessWidget {
+  const SellerHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("seller home screen"),),
+    );
+  }
+}
