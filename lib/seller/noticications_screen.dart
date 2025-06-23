1c013537d521e3737386dde5f3853ddff8233c61
Morison <morisonabinsinguza@gmail.com>
2025-06-23 23:17:13 +0300

    notification screen

---
+++ lib/seller/noticications_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoticicationsScreen extends StatelessWidget {
+  const NoticicationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
