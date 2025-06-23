9e207733aaa171dadaee142db1595710e42cd288
Morison <morisonabinsinguza@gmail.com>
2025-06-23 16:46:00 +0300

    add drawer for navigation

--- lib/seller/seller_home_screen.dart
+++ lib/seller/seller_home_screen.dart
@@ -6,7 +6,34 @@ class SellerHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("seller home screen"),),
+      appBar: AppBar(backgroundColor: Colors.white),
+      body: Center(child: Text("seller home screen")),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/logo.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Text("Hello seller"),
+            ),
+
+            ListTile(leading: Icon(Icons.dashboard), title: Text("Dashboard")),
+
+            ListTile(
+              leading: Icon(Icons.apartment),
+              title: Text("My Listings"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/mylistings_screen');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
