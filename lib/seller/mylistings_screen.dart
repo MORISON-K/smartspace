b1b8cf98ce6f859d240463f54137a5495c05d33a
Morison <morisonabinsinguza@gmail.com>
2025-06-23 16:47:12 +0300

    add seller listings screen

---
+++ lib/seller/mylistings_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MylistingsScreen extends StatelessWidget {
+  const MylistingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
