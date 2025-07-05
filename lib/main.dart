7df10870a386e084129ea792a9e0fc9c4a02848f
Morison <morisonabinsinguza@gmail.com>
2025-07-05 20:33:51 +0300

    Files moved to notifications folder

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:smartspace/seller/my_listings_screen.dart';
 import 'package:smartspace/seller/add_listing_screen.dart';
 import 'package:smartspace/seller/ai-valuation/ai_valuation.dart';
 import 'package:smartspace/seller/analytics_screen.dart';
-import 'package:smartspace/seller/noticications_screen.dart';
+import 'package:smartspace/notifications/notifications_screen.dart';
 import 'package:smartspace/seller/profile_screen.dart';
 import 'package:smartspace/seller/seller_home_screen.dart';
 
@@ -33,7 +33,7 @@ class SmartSpace extends StatelessWidget {
         '/mylistings_screen': (context) => const MyListingsScreen(),
         '/analytics_screen': (context) => AnalyticsScreen(),
         '/ai_valuation': (context) => AiValuationScreen(),
-        '/notifications_screen': (context) => NoticicationsScreen(),
+        '/notifications_screen': (context) => NotificationsScreen(),
         '/add_listing_screen': (context) => AddListingScreen(),
         '/seller_home_screen': (context) => SellerHomeScreen(),
         '/profile_screen': (context) => ProfileScreen(),
