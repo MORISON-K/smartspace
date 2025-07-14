c6fe3107be2241570a947738cd8369dea04c5320
biar211 <elijahbiar2@gmail.com>
2025-07-14 20:21:39 +0300

    created this new file of details and added some new features in it

---
+++ lib/seller/listing_detail_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ListingDetailScreen extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String price;
+  final String location;
+  final String description;
+
+  const ListingDetailScreen({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+    required this.location,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listing Details"),
+        backgroundColor: Color.fromARGB(255, 164, 192, 221),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            imageUrl.isNotEmpty
+                ? ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      imageUrl,
+                      height: 220,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                : Icon(Icons.image_not_supported, size: 100),
+            const SizedBox(height: 16),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              '💰 Price: \$$price',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '📍 Location: $location',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              '📝 Description:',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
