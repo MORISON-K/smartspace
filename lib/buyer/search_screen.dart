90ed035f45bad5967ec60dd805637c1939c73bef
Lunkuse Dorcus <dorcusjane2003@gmail.com>
2025-07-25 12:35:13 +0300

    Update search_screen.dart

--- lib/buyer/search_screen.dart
+++ lib/buyer/search_screen.dart
@@ -150,7 +150,7 @@ class _SearchScreenState extends State<SearchScreen> {
               },
             ),
             icon: BitmapDescriptor.defaultMarkerWithHue(
-              BitmapDescriptor.hueGreen,
+              BitmapDescriptor.hueBlue,
             ),
           );
 
